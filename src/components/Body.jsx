5ad1ec6e2f63aa8e9e49a83abf938a94adb12f39
Ashutosh-aky-2004 <ashutoshyadav202004@gmail.com>
2025-08-02 20:01:50 +0530

    fixed bugs

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,9 @@
-import { useEffect } from "react";
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import appStore from "../utils/appStore";
-import { Provider, useDispatch } from "react-redux";
-import { userSignIn, userSignOut } from "../utils/userSlice";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
+import { Provider } from "react-redux";
 
-// Create the router outside to avoid recreation on every render
 const appRouter = createBrowserRouter([
   {
     path: "/",
@@ -20,35 +15,10 @@ const appRouter = createBrowserRouter([
   },
 ]);
 
-// Create a component with access to dispatch
-const AuthWrapper = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        dispatch(
-          userSignIn({
-            displayName: user.displayName,
-            email: user.email,
-            photoURL: user.photoURL,
-          })
-        );
-      } else {
-        dispatch(userSignOut());
-      }
-    });
-
-    return () => unsubscribe(); // cleanup on unmount
-  }, [dispatch]);
-
-  return <RouterProvider router={appRouter} />;
-};
-
 const Body = () => {
   return (
     <Provider store={appStore}>
-      <AuthWrapper />
+      <RouterProvider router={appRouter} />;
     </Provider>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,35 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/Netflix-Logo-Streaming-Platform-765.png";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { userSignIn, userSignOut } from "../utils/userSlice";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          userSignIn({
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(userSignOut());
+        navigate("/");
+      }
+    });
+      //unsubscribe when the component unmount
+    return () => unsubscribe();
+  }, []);
+  
   return (
     <div className="absolute top-0 left-0 w-full px-8 py-2 z-20 bg-gradient-to-b from-black">
       <img className="w-40" src={logo} alt="Netflix Logo" />
